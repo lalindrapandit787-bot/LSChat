d4901c49aedded16f8679b40a933c2d4fa37d05e
LALINDAR PANDIT <lalindrapandit787@gmail.com>
2026-08-23 01:09:57 +0545

    Fix AppRegistryNotReady in asgi.py

--- ludochat/asgi.py
+++ ludochat/asgi.py
@@ -1,7 +1,25 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","ludochat.settings")
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+import django
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
-application=ProtocolTypeRouter({"http":get_asgi_application(),"websocket":AuthMiddlewareStack(URLRouter(websocket_urlpatterns))})
+
+# 1. Django Settings Modules Set गर्ने
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludochat.settings')
+
+# 2. AppRegistryNotReady Error हटाउन पहिले Django setup गर्ने
+django.setup()
+
+# 3. get_asgi_application() र Channels Routing Import गर्ने
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing  # तपाईंको routing file
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
